c92cabb7c87282148b5caf3678c2c65c3a499d5d
Donald Steinert <steinert.donald@gmail.com>
2015-02-16 21:47:34 -0500

    added html, css; specified routes for signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'pages#signup'
+  get 'pages/signup'
+  
+  match 'signup', :to => 'pages#signup', :via => :get
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
